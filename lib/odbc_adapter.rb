f79baa0a47754e0ae443927d47490b31f780e8e5
Kevin Murphy <kevin@bookbub.com>
2024-11-06 15:49:30 -0500

    Register Adapter with ActiveRecord
    Rails 7.2 [introduced](https://github.com/rails/rails/blame/v7.2.0/activerecord/lib/active_record/connection_adapters.rb#L22C13-L22C13)
    a [new API](https://github.com/rails/rails/commit/009c7e74117690f0dbe200188a929b345c9306c1) to register AR adapters.
    
    Rails 7.2 fails to recognize this adapter without any changes.
    
    ```
    ActiveRecord::AdapterNotFound: Database configuration specifies nonexistent 'odbc' adapter. Available adapters are: mysql2, postgresql, sqlite3, trilogy. Ensure that the adapter is spelled correctly in config/database.yml and that you've added the necessary adapter gem to your Gemfile and that it is at its latest version. If it is up to date, the adapter may need to be modified. (ActiveRecord::AdapterNotFound)
    ```
    
    After finding the new register method while reading the resolve method
    to determine where the exception is getting raised, I looked for other
    adapters to see how they are registering. I found an [oracle adapter](https://github.com/rsim/oracle-enhanced/commit/8d9aef94350eb5a8b361d7f2751d16c03ae06653#diff-2a303f262be2323c57acc494e83d5e9f3ece612109d14bda58b2aa392e3c9320R14)
    using this new registration, when it's available, and used that to
    cross-reference my implementation.

--- lib/odbc_adapter.rb
+++ lib/odbc_adapter.rb
@@ -1,2 +1,12 @@
-# Requiring with this pattern to mirror ActiveRecord
-require 'active_record/connection_adapters/odbc_adapter'
+ActiveSupport.on_load(:active_record) do
+  # Requiring with this pattern to mirror ActiveRecord
+  require 'active_record/connection_adapters/odbc_adapter'
+
+  if ActiveRecord::ConnectionAdapters.respond_to?(:register)
+    ActiveRecord::ConnectionAdapters.register(
+      "odbc",
+      "ActiveRecord::ConnectionAdapters::ODBCAdapter",
+      "active_record/connection_adapters/odbc_adapter"
+    )
+  end
+end
