ef6ff1ef6d9dd03dcbe96f6e34ce089496408654
Kevin Murphy <kevin@bookbub.com>
2024-11-19 10:54:55 -0500

    use assert_empty

--- test/metadata_test.rb
+++ test/metadata_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class MetadataTest < Minitest::Test
   def test_data_sources
-    assert_true (%w[todos users] - User.connection.data_sources.sort).empty?
+    assert_empty (%w[todos users] - User.connection.data_sources.sort)
   end
 
   def test_column_names
