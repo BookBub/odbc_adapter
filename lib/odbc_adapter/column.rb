0e081d53dcb64eb4d571b8779085a0acf2a46160
Kevin Murphy <kevin@bookbub.com>
2024-11-22 10:51:24 -0500

    back out auto incremented

--- lib/odbc_adapter/column.rb
+++ lib/odbc_adapter/column.rb
@@ -1,14 +1,13 @@
 module ODBCAdapter
   class Column < ActiveRecord::ConnectionAdapters::Column
-    attr_reader :native_type, :auto_incremented
+    attr_reader :native_type
 
     # Add the native_type accessor to allow the native DBMS to report back what
     # it uses to represent the column internally.
     # rubocop:disable Metrics/ParameterLists
-    def initialize(name, default, sql_type_metadata = nil, null = true, native_type = nil, auto_incremented, **kwargs)
+    def initialize(name, default, sql_type_metadata = nil, null = true, native_type = nil, **kwargs)
       super(name, default, sql_type_metadata, null, **kwargs)
       @native_type = native_type
-      @auto_incremented = auto_incremented
     end
   end
 end
