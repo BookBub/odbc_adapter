f8c2abae7fd20eb84afe93219e1dfdd2b5a11785
Kevin Murphy <kevin@bookbub.com>
2024-11-22 14:08:11 -0500

    visiting visit?

--- lib/odbc_adapter/database_statements.rb
+++ lib/odbc_adapter/database_statements.rb
@@ -235,6 +235,52 @@ end
 
 module Arel # :nodoc: all
   module Visitors
+    class Visitor
+      def initialize
+        @dispatch = get_dispatch_cache
+      end
+
+      def accept(object, collector = nil)
+        visit object, collector
+      end
+
+      private
+        attr_reader :dispatch
+
+        def self.dispatch_cache
+          @dispatch_cache ||= Hash.new do |hash, klass|
+            hash[klass] = "visit_#{(klass.name || '').gsub('::', '_')}"
+          end
+        end
+
+        def get_dispatch_cache
+          self.class.dispatch_cache
+        end
+
+        def visit(object, collector = nil)
+          puts "IN AREL VISITOR#VISIT"
+          dispatch_method = dispatch[object.class]
+          puts "dispatch_method: #{dispatch_method}"
+          if collector
+            send dispatch_method, object, collector
+          else
+            send dispatch_method, object
+          end
+        rescue NoMethodError => e
+          raise e if respond_to?(dispatch_method, true)
+          superklass = object.class.ancestors.find { |klass|
+            respond_to?(dispatch[klass], true)
+          }
+          raise(TypeError, "Cannot visit #{object.class}") unless superklass
+          dispatch[object.class] = dispatch[superklass]
+          retry
+        end
+    end
+  end
+end
+
+module Arel # :nodoc: all
+  module Visitors
     class UnsupportedVisitError < StandardError
       def initialize(object)
         super "Unsupported argument type: #{object.class.name}. Construct an Arel node instead."
