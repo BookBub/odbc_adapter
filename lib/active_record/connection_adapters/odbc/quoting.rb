e11411f290a2b1397d3f6766fce27235743c61d1
Kevin Murphy <kevin@bookbub.com>
2024-11-25 15:28:51 -0500

    try to get DB metadata out of concern

--- lib/active_record/connection_adapters/odbc/quoting.rb
+++ lib/active_record/connection_adapters/odbc/quoting.rb
@@ -8,7 +8,7 @@ module ActiveRecord
           # Returns a quoted form of the column name.
           def quote_column_name(name)
             name = name.to_s
-            quote_char = database_metadata.identifier_quote_char.to_s.strip
+            quote_char = self.database_metadata.identifier_quote_char.to_s.strip
 
             return name if quote_char.length.zero?
             quote_char = quote_char[0]
@@ -17,7 +17,7 @@ module ActiveRecord
             return name if name[0] == quote_char && name[-1] == quote_char
 
             # If upcase identifiers, only quote mixed case names.
-            if database_metadata.upcase_identifiers?
+            if self.database_metadata.upcase_identifiers?
               return name unless name =~ /([A-Z]+[a-z])|([a-z]+[A-Z])/
             end
 
