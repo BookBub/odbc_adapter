fca840448acd9b042ad93682039846813974abc3
Kevin Murphy <kevin@bookbub.com>
2024-11-25 14:58:39 -0500

    quoted column name in class methods

--- lib/active_record/connection_adapters/odbc/quoting.rb
+++ lib/active_record/connection_adapters/odbc/quoting.rb
@@ -7,23 +7,25 @@ module ActiveRecord
           string.gsub(/\'/, "''")
         end
 
-        # Returns a quoted form of the column name.
-        def quote_column_name(name)
-          name = name.to_s
-          quote_char = database_metadata.identifier_quote_char.to_s.strip
+        module ClassMethods # :nodoc:
+          # Returns a quoted form of the column name.
+          def quote_column_name(name)
+            name = name.to_s
+            quote_char = database_metadata.identifier_quote_char.to_s.strip
 
-          return name if quote_char.length.zero?
-          quote_char = quote_char[0]
+            return name if quote_char.length.zero?
+            quote_char = quote_char[0]
 
-          # Avoid quoting any already quoted name
-          return name if name[0] == quote_char && name[-1] == quote_char
+            # Avoid quoting any already quoted name
+            return name if name[0] == quote_char && name[-1] == quote_char
 
-          # If upcase identifiers, only quote mixed case names.
-          if database_metadata.upcase_identifiers?
-            return name unless name =~ /([A-Z]+[a-z])|([a-z]+[A-Z])/
+            # If upcase identifiers, only quote mixed case names.
+            if database_metadata.upcase_identifiers?
+              return name unless name =~ /([A-Z]+[a-z])|([a-z]+[A-Z])/
+            end
+
+            "#{quote_char.chr}#{name}#{quote_char.chr}"
           end
-
-          "#{quote_char.chr}#{name}#{quote_char.chr}"
         end
 
         # Ideally, we'd return an ODBC date or timestamp literal escape
