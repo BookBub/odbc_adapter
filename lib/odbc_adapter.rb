8714427d90c9e52b3b9903769e8ae962402f1121
Kevin Murphy <kevin@bookbub.com>
2024-11-25 13:43:16 -0500

    this gem doesn't have ActiveSupport, but you probably do in your Rails app

--- lib/odbc_adapter.rb
+++ lib/odbc_adapter.rb
@@ -1,4 +1,16 @@
-ActiveSupport.on_load(:active_record) do
+if defined?(ActiveSupport)
+  ActiveSupport.on_load(:active_record) do
+    # Requiring with this pattern to mirror ActiveRecord
+    require 'active_record/connection_adapters/odbc_adapter'
+    if ActiveRecord::ConnectionAdapters.respond_to?(:register)
+      ActiveRecord::ConnectionAdapters.register(
+        "odbc",
+        "ActiveRecord::ConnectionAdapters::ODBCAdapter",
+        "active_record/connection_adapters/odbc_adapter"
+      )
+    end
+  end
+else
   # Requiring with this pattern to mirror ActiveRecord
   require 'active_record/connection_adapters/odbc_adapter'
   if ActiveRecord::ConnectionAdapters.respond_to?(:register)
