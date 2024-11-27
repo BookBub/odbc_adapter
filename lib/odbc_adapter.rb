b03d2611da0c399d51a9ebacd88d8265a4a1e2b5
Kevin Murphy <kevin@bookbub.com>
2024-11-27 10:26:24 -0500

    CI doesn't have AS

--- lib/odbc_adapter.rb
+++ lib/odbc_adapter.rb
@@ -1,7 +1,19 @@
-ActiveSupport.on_load(:active_record) do
+if defined?(ActiveSupport)
+  ActiveSupport.on_load(:active_record) do
+    # Requiring with this pattern to mirror ActiveRecord
+    require 'active_record/connection_adapters/odbc_adapter'
+
+    if ActiveRecord::ConnectionAdapters.respond_to?(:register)
+      ActiveRecord::ConnectionAdapters.register(
+        "odbc",
+        "ActiveRecord::ConnectionAdapters::ODBCAdapter",
+        "active_record/connection_adapters/odbc_adapter"
+      )
+    end
+  end
+else
   # Requiring with this pattern to mirror ActiveRecord
   require 'active_record/connection_adapters/odbc_adapter'
-
   if ActiveRecord::ConnectionAdapters.respond_to?(:register)
     ActiveRecord::ConnectionAdapters.register(
       "odbc",
