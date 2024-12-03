5d7efa0d1db742dcec9eb463866c45fa753681c9
zjohl <zamir@bookbub.com>
2024-12-03 15:06:56 -0500

    Create connection on init, and other changes

--- lib/odbc_adapter/adapters/postgresql_odbc_adapter.rb
+++ lib/odbc_adapter/adapters/postgresql_odbc_adapter.rb
@@ -131,6 +131,10 @@ module ODBCAdapter
         "DISTINCT #{columns}, #{order_columns * ', '}"
       end
 
+      def primary_key_type
+        PRIMARY_KEY
+      end
+
       def connected?
         !@raw_connection.nil?
       end
