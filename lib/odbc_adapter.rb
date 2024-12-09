0d5fbf6598fc96b6bce641b200defff61733b377
zjohl <zamir@bookbub.com>
2024-12-09 09:16:23 -0500

    Add explicit null adapter

--- lib/odbc_adapter.rb
+++ lib/odbc_adapter.rb
@@ -2,9 +2,15 @@
 require 'active_record/connection_adapters/odbc_adapter'
 require 'odbc_adapter/adapters/postgresql_odbc_adapter'
 require 'odbc_adapter/adapters/mysql_odbc_adapter'
+require 'odbc_adapter/adapters/null_odbc_adapter'
 if defined?(ActiveSupport)
   ActiveSupport.on_load(:active_record) do
     if ActiveRecord::ConnectionAdapters.respond_to?(:register)
+      ActiveRecord::ConnectionAdapters.register(
+        "odbc",
+        "ODBCAdapter::Adapters::NullODBCAdapter",
+        "odbc_adapter/adapters/null_odbc_adapter"
+      )
       ActiveRecord::ConnectionAdapters.register(
         "postgres_odbc",
         "ODBCAdapter::Adapters::PostgreSQLODBCAdapter",
