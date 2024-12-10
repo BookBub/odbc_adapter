d6725db0b34981ed246d33cb8f2a88ccf7a385b1
zjohl <zamir@bookbub.com>
2024-12-10 09:59:20 -0500

    Add instructions for keeping version updated

--- lib/odbc_adapter/version.rb
+++ lib/odbc_adapter/version.rb
@@ -1,3 +1,11 @@
 module ODBCAdapter
+
+  # When updating this number, please do the following:
+  # Before merging changes:
+  # - Update the CHANGELOG.md with details about this change
+  # After merging changes:
+  # - Create a tag for this release:
+  #   1. `git tag v{YOUR-VERSION-NUMBER}`
+  #   2. `git push origin --tags`
   VERSION = '10.1.0'.freeze
 end
