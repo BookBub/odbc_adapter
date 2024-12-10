5a3d27539e77f8978582e96d499db5796c2004b2
zjohl <zamir@bookbub.com>
2024-12-10 09:03:54 -0500

    Clean up adapter registration

--- lib/odbc_adapter.rb
+++ lib/odbc_adapter.rb
@@ -3,27 +3,8 @@ require 'active_record/connection_adapters/odbc_adapter'
 require 'odbc_adapter/adapters/postgresql_odbc_adapter'
 require 'odbc_adapter/adapters/mysql_odbc_adapter'
 require 'odbc_adapter/adapters/null_odbc_adapter'
-if defined?(ActiveSupport)
-  ActiveSupport.on_load(:active_record) do
-    if ActiveRecord::ConnectionAdapters.respond_to?(:register)
-      ActiveRecord::ConnectionAdapters.register(
-        "odbc",
-        "ODBCAdapter::Adapters::NullODBCAdapter",
-        "odbc_adapter/adapters/null_odbc_adapter"
-      )
-      ActiveRecord::ConnectionAdapters.register(
-        "postgres_odbc",
-        "ODBCAdapter::Adapters::PostgreSQLODBCAdapter",
-        "odbc_adapter/adapters/postgresql_odbc_adapter"
-      )
-      ActiveRecord::ConnectionAdapters.register(
-        "mysql_odbc",
-        "ODBCAdapter::Adapters::MySQLODBCAdapter",
-        "odbc_adapter/adapters/mysql_odbc_adapter"
-      )
-    end
-  end
-else
+
+def register_adapters
   if ActiveRecord::ConnectionAdapters.respond_to?(:register)
     ActiveRecord::ConnectionAdapters.register(
       "odbc",
@@ -41,4 +22,12 @@ else
       "odbc_adapter/adapters/mysql_odbc_adapter"
     )
   end
+end
+
+if defined?(ActiveSupport)
+  ActiveSupport.on_load(:active_record) do
+    register_adapters
+  end
+else
+  register_adapters
 end
